0cec9524f67d054b7d82d9570616cbefbd8c8d24
ajstimson <ajstimson@gmail.com>
2022-08-03 18:10:23 -0700

    initial commit

---
+++ Pre-Course Work/STIMSON_ANDREW_JSPredict.js
@@ -0,0 +1,10 @@
+//I was born in 1980
+function myBirthYearFunc() {
+  console.log("I was born in " + 1980);
+}
+
+const birthYearInput = 1980;
+//I was born in 1980
+function myBirthYearFunc(birthYearInput) {
+  console.log("I was born in " + birthYearInput);
+}
---
+++ Pre-Course Work/STIMSON_ANDREW_JSPredictSum.js
@@ -0,0 +1,13 @@
+/*1,2
+ * Summing Numbers!
+ * num1 is 1
+ * num2 is 2
+ * 3
+ */
+function add(num1, num2) {
+  console.log("Summing Numbers!");
+  console.log("num1 is: " + num1);
+  console.log("num2 is: " + num2);
+  var sum = num1 + num2;
+  console.log(sum);
+}
