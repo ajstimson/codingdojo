e46363a7dc90873837c4289a9279e1e73470c02a
Andrew Stimson <andrew@applejuice.codes>
2022-08-08 21:23:34 -0700

    JS: Fizzbuzz

---
+++ Week 3/Fizzbuzz/script.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 100; i++) {
+  const result =
+    i % 3 === 0 && i % 5 === 0
+      ? "FizzBuzz"
+      : i % 3 === 0
+      ? "Fizz"
+      : i % 5 === 0
+      ? "Buzz"
+      : i;
+  console.log(result);
+}
