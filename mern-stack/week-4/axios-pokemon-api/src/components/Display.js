b3e44e103b2d95836bebf0c139295d0bb85d6bd1
Andrew Stimson <andrew@applejuice.codes>
2022-09-19 14:28:17 -0700

    MERN: Axios Pokemon API (fix number of requested pokes)

--- mern-stack/week-4/axios-pokemon-api/src/components/Display.js
+++ mern-stack/week-4/axios-pokemon-api/src/components/Display.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-const API = "https://pokeapi.co/api/v2/pokemon?limit=400&offset=0"
+const API = "https://pokeapi.co/api/v2/pokemon?limit=807&offset=0"
 
 const Display = (props) => {
 	const [pokemon, setPokemon] = useState([])
