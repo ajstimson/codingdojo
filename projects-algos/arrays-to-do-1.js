7450cc0442980c8fcfaadfd5271edde5892d2766
Andrew Stimson <andrew@applejuice.codes>
2022-10-28 12:29:54 -0700

    Project Algos: Arrays to Do 1 (Insert At)

--- projects-algos/arrays-to-do-1.js
+++ projects-algos/arrays-to-do-1.js
@@ -41,3 +41,25 @@ const popFront = (arr) => {
 
 console.log("1. popFront returns:", popFront([0, 5, 10, 15]))
 console.log("2. popFront returns:", popFront([4, 5, 7, 9]))
+
+/*
+* * * Insert At * * * 
+* Given an array, index, and additional value, 
+* insert the value into array at given index. 
+* You can think of pushFront(arr,val) as equivalent 
+* to insertAt(arr,0,val). You may use .push(), you 
+* are able do this without it though!
+
+? Examples:
+
+! insertAt([100,200,5], 2, 311) => [100,200,311,5]
+! insertAt([9,33,7], 1, 42) => [9,42,33,7] 
+*/
+
+const insertAt = (arr, i, val) => {
+	arr.splice(i, 0, val)
+	return arr
+}
+
+console.log("1. insertAt", insertAt([100, 200, 5], 2, 311))
+console.log("2. insertAt", insertAt([9, 33, 7], 1, 42))
